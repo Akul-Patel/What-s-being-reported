2405f670a546692af1e147afd366abf764f2c240
Akulkumar Patel <avp76@njit.edu>
2021-02-16 03:39:26 +0000

    added deleting DOM element

--- static/script.js
+++ static/script.js
@@ -9,17 +9,22 @@ window.onload = () => { // onload makes sure the content is loaded on page first
             .then(data => { // .then will only run the function *once* the data is fetched from the internet
                 console.log(data); // See what this logs!   
                 var i;
+                
+                let getting_data = document.getElementById('new');
+                
+                while (getting_data.firstChild) {
+                    getting_data.removeChild(getting_data.firstChild);
+                }
+                    
                 for (i = 0; i < data["headlines"].length; i++) {
-                    var tag = document.createElement("p");
-                    var text = document.createTextNode(data["headlines"][i] + data["dates"][i]);
-                    tag.appendChild(text);
-                    var element = document.getElementById("new");
-                    element.appendChild(tag);
-                    var tag2 = document.createElement("p");
-                    var text2 = document.createTextNode(data["snippets"][i]);
-                    tag2.appendChild(text2);
-                    var element2 = document.getElementById("new2");
-                    element2.appendChild(tag2);
+                    let newDiv = document.createElement('div');
+                    let headlines = document.createElement('b');
+                    let article = document.createElement('p');
+                    headlines.textContent = data['headlines'][i].link(data['urls'][i]) + "(" + data['dates'][i] + ")";
+                    newDiv.appendChild(headlines);
+                    article.textContent = data['snippets'][i];
+                    newDiv.append(headlines, article);
+                    getting_data.appendChild(newDiv); 
                 }
             });
     });
