306260667ee9b4db4135c369a5bea67ad75f8cd4
Akul <akul24700@gmail.com>
2021-11-15 14:45:04 -0500

    Fixed the problem with the title(link) on search

--- static/script.js
+++ static/script.js
@@ -9,21 +9,36 @@ window.onload = () => { // onload makes sure the content is loaded on page first
             .then(data => { // .then will only run the function *once* the data is fetched from the internet
                 console.log(data); // See what this logs!   
                 var i;
+                let getting_title = document.getElementById('title');
                 
+                while (getting_title.firstChild) {
+                    getting_title.removeChild(getting_title.firstChild);
+                }
+                let newH1 = document.createElement('h7');
+                newH1.textContent = "What's being reported about " + userText + "?";
+                getting_title.append(newH1); 
+
                 let getting_data = document.getElementById('new');
                 
                 while (getting_data.firstChild) {
                     getting_data.removeChild(getting_data.firstChild);
                 }
+
                     
                 for (i = 0; i < data["headlines"].length; i++) {
                     let newDiv = document.createElement('div');
-                    let headlines = document.createElement('b');
+                    let headlines = document.createElement('a');
+                    let date = document.createElement('p');
+                    var linkText = document.createTextNode(data['headlines'][i]);
                     let article = document.createElement('p');
-                    headlines.textContent = data['headlines'][i].link(data['urls'][i]) + "(" + data['dates'][i] + ")";
-                    newDiv.appendChild(headlines);
+                    // -----------------------
+                    headlines.appendChild(linkText);
+                    headlines.title = data['headlines'][i];
+                    headlines.href = data['urls'][i];
+                    // -----------------------
+                    date.textContent = "(" + data['dates'][i] + ")"
                     article.textContent = data['snippets'][i];
-                    newDiv.append(headlines, article);
+                    newDiv.append(headlines, date, article);
                     getting_data.appendChild(newDiv); 
                 }
             });
